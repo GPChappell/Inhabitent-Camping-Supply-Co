7f059407e63e17a43c6f6cab9c5d08d2ec668ad2
Guy Chappell <mettcat@gmail.com>
2018-02-22 20:26:02 -0800

    Footer layout and styling complete. Hardcoded content

--- themes/inhabitent/footer.php
+++ themes/inhabitent/footer.php
@@ -8,12 +8,36 @@
 ?>
 
 			</div><!-- #content -->
-
 			<footer id="colophon" class="site-footer" role="contentinfo">
-				<div class="site-info">
-					<div class="footer-entry">
-						
+				<div class="footer-inner content-width">
+					<div class="footer-block-area">
+
+						<div class="footer-block footer-contact-info">
+							<h2>Contact Info</h2>
+							<p><a href="mailto:info@inhabitent.com"><i class="fas fa-envelope "></i>info@inhabitent.com</a></p>
+							<p><a href="tel:778-456-7891"><i class="fas fa-phone fa-flip-horizontal"></i>778-456-7891</a></p>
+							<a class="social-icon-link" href="" target="_blank"><i class="fab fa-facebook-square fa-lg"></i></a>
+							<a class="social-icon-link" href="" target="_blank"><i class="fab fa-twitter-square fa-lg"></i></a>
+							<a class="social-icon-link" href="" target="_blank"><i class="fab fa-google-plus-square fa-lg"></i></a>
+						</div>
+
+						<div class="footer-block footer-business-hours">
+							<h2>Business Hours</h2>
+							<p><strong>Monday-Friday:</strong> 9am to 5pm</p>
+							<p><strong>Saturday:</strong> 10am to 2pm</p>
+							<p><strong>Sunday:</strong> Closed</p>
+						</div>
+					
+
+						<div class="footer-block footer-site-logo">
+							<a href="<?php echo get_home_url() ?>"><img src="<?php echo get_template_directory_uri() . '/images/' ?>inhabitent-logo-text.svg" alt="Inhabitent Camping Supply Co Logo"/></a>
+						</div>
 					</div>
+
+					<div class="footer-copyright-info">
+						<p>Copyright &copy; 2018 Inhabitent</p>
+					</div>
+
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
