a83d065892c22a52609fc85610280ff97dc6f023
Guy Chappell <mettcat@gmail.com>
2018-02-26 16:04:36 -0800

    About page done.

--- themes/inhabitent/about.php
+++ themes/inhabitent/about.php
@@ -10,12 +10,15 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<div class="hero-image-full-page">
 			<div class="header-title-wrapper">
-				<h1><?php echo get_the_title(16);?></h1>
+				<h1><?php echo get_the_title( get_the_ID() );?></h1>
 			</div>
 		</div>
-		<main id="main" class="site-main" role="main">
-			<div>
-				<p><?php echo CFS()->get( 'our_story' ); ?></p>
+		<main id="main" class="site-main fixed-width-content" role="main">
+			<div class="static-page-text-wrapper" >
+				<?php echo CFS()->get( 'our_story' ); ?>
+			</div>
+			<div class="static-page-text-wrapper" >
+				<?php echo CFS()->get( 'our_team' ); ?>
 			</div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
