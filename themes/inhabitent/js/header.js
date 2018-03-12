fc1eb382ec1661be65045f8b8b32d955ad2ea23c
Guy Chappell <mettcat@gmail.com>
2018-03-11 20:42:29 -0700

    Header search box styling updated

--- themes/inhabitent/js/header.js
+++ themes/inhabitent/js/header.js
@@ -13,15 +13,30 @@
     $(window).scroll( function() {
       if( $(window).scrollTop() === 0 ) {
         $('.site-header').css('opacity','1');
-        $('.site-header').addClass('reverse-header');
+        // $('.site-header').addClass('reverse-header');
       }
       else if( $(window).scrollTop() > 0 && $(window).scrollTop() < heroBannerHeight ) {
           $('.site-header').css('opacity','0');
+          $('.site-header').addClass('reverse-header');
       }
       else if( $(window).scrollTop() > heroBannerHeight) {
         $('.site-header').css('opacity','1');
         $('.site-header').removeClass('reverse-header');
       }
     })
+
+
+    // $(window).scroll( function() {
+    //   if( $(window).scrollTop() > 0 
+    //     && $(window).scrollTop() < heroBannerHeight ) {
+    //       $('.site-header').css('opacity','0');
+    //       $('.site-header').addClass('reverse-header');
+    //   }
+    //   else if( $(window).scrollTop() > heroBannerHeight) {
+    //     $('.site-header').css('opacity','1');
+    //     $('.site-header').removeClass('reverse-header');
+    //   }
+    // });
+    
   }
 } )(jQuery);
